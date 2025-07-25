b115ceb27c72b93f64b70ce7cb6e5e6521d2e4b1
AdityKumari09 <kumariadity09@gmail.com>
2025-07-25 13:54:02 +0530

    new commit

---
+++ 012_final/Test.java
@@ -0,0 +1,15 @@
+final class Vehicle{ //final class(can't be extended)
+    final int maxSpeed = 120; //final variable(can't be reassigned)
+
+    public final void showSpeed(){ //final method(can't be overridden in subclass)
+        System.out.println("Max soeed :- " + maxSpeed + " km/h");
+    }
+}
+
+public class Test {
+    public static void main(String[] args) {
+        Vehicle v = new Vehicle();
+        // v.maxSpeed = 150; //Error: cannot reassign a value to final variable
+        v.showSpeed();
+    }
+}
